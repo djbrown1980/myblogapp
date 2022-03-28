10e23e614ae68db6109f2ba7fe62c570289b4f10
David Brown <98230004+djbrown1980@users.noreply.github.com>
2022-03-28 15:49:17 +0000

    changed the run command to execute run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ "$REPL_SLUG" == 'blog_dev']
+then
+  hugo serve --buildDrafts --buildFuture --bind 0.0.0.0 --port 443 --baseURL https://$REPL_SLUG.$REPL_OWNER.repl.co
+else
+  hugo serve --bind 0.0.0.0 --port 443 --baseURL https://$REPL_SLUG.$REPL_OWNER.repl.co
+fi
